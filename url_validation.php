9376ac075005647a4b126b17c01cf491a8dd34d9
osk2 <sc60714@gmail.com>
2015-10-21 19:03:58 +0800

    suppress error message from url_validation.php

--- url_validation.php
+++ url_validation.php
@@ -3,7 +3,7 @@
 	if (isset($_GET['url']) && !empty($_GET['url'])) {
 		$url = $_GET['url'];
 		
-		if(getimagesize($url) !== false) {
+		if(@getimagesize($url) !== false) {
 			echo json_encode(array('result' => 'ok'));
 		}else{
 			echo json_encode(array('result' => 'fail'));
